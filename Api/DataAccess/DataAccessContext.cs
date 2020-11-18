2e07d5b13298e019989769036d39549d3783aa30
VillakaziCreator <villakazi.nhlanhla@gmail.com>
2020-11-18 08:12:38 +0200

    Added the project to git

---
+++ Api/DataAccess/DataAccessContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.DataAccess
+{
+    public class DataAccessContext : DbContext
+    {
+        public DataAccessContext(DbContextOptions<DataAccessContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().HasData(new Student
+            {
+                StudentNumber = "21958451",
+                FirstName = "Nhlanhla"
+            });
+
+        }
+    }
+}
